f437536b16deabf92431ae7fdcf67e87bd97339b
schneiderfelipe <schneiderfelipe@users.noreply.github.com>
2021-07-30 22:14:16 +0000

    deploy: 3d8e6cbe2443ce6b26ec45534a71b6558169c347

--- mermaid-init.js
+++ mermaid-init.js
@@ -1,1 +1,8 @@
-mermaid.initialize({startOnLoad:true});
+mermaid.initialize({
+    startOnLoad: true,
+    theme: "base",
+    fontFamily: "'Open Sans', sans-serif",
+    themeVariables: {
+        primaryColor: "#1E152A",
+    },
+});
